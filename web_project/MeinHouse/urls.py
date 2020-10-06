c0ce9580fabbb6e7e3db5a7638f6f2a6ec76bb04
Hassan Mata <ajdourhassan29@gmail.com>
2020-10-06 13:56:54 +0200

    save new changes

--- web_project/MeinHouse/urls.py
+++ web_project/MeinHouse/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
    path('', views.home , name="home"),
-   path('about/' , views.about , name="about"),
+   path('about_page' , views.about , name='about'),
+   path('dashbord' , views.dashbord , name='dashbord')
 ]
